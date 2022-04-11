0442ddd2ac3bfb61bc94c6ff72a270f1641116aa
Thai Tran <1tranquocthai@gmail.com>
2022-04-11 15:31:46 +0700

    update

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SoftwareEngineering.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
