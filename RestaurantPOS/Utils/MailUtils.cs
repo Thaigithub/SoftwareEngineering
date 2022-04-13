75fa36bf3397eaf2b8b963e83fc87901db23a833
Thagithub <1tranquocthai@gmail.com>
2022-04-13 13:51:02 +0700

    Update

--- RestaurantPOS/Utils/MailUtils.cs
+++ RestaurantPOS/Utils/MailUtils.cs
@@ -28,8 +28,8 @@ namespace RestaurantPOS.Utils
                 await client.SendMailAsync(message);
                 return true;
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
+                
                 return false;
             }
         }
